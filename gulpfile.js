30577be0bb85a893c28541a6f8da2d8c9305b34b
Maciej Adamski <maciej.adamski89@gmail.com>
2016-11-23 21:09:43 +0100

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+var gulp = require('gulp');
+var jade = require('gulp-jade');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+
+gulp.task('jade', function() {
+    return gulp.src('src/**/*.jade')
+        .pipe(jade())
+        .pipe(gulp.dest('dest'));
+});
+
+gulp.task('sass', function()
+{
+  gulp.src('src/sass/**/*.sass')
+  .pipe(sass().on('error', sass.logError))
+  .pipe(gulp.dest('dest/css/'))
+  .pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task('browser-sync', function()
+{
+  browserSync.init(
+  {
+    server:
+    {
+      baseDir: "dest/"
+    }
+ });
+});
+
+// Gulp Watch Task
+gulp.task('watch', ['browser-sync'], function ()
+{
+  gulp.watch('src/**/*.jade', ['jade']);
+  gulp.watch('src/**/*.sass', ['sass']);
+  gulp.watch('dest/*.html').on('change', browserSync.reload);
+  gulp.watch('dest/*.css').on('change', browserSync.reload);
+});
+
+// Gulp Default Task
+gulp.task('default', ['watch']);
+
+
